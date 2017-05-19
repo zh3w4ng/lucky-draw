64bf6578e6a6ccc1a9d2e74450d27f6b6945c02b
Zhe Wang <zhe_wang@singaporeair.com.sg>
2017-05-19 19:07:34 +0800

    tweak converting

--- convert.rb
+++ convert.rb
@@ -2,15 +2,16 @@
 require 'csv'
 require 'json'
 list = {}
-CSV.foreach("list.csv") do |row|
+CSV.foreach("list.csv", quote_char: '"') do |row|
   # use row here...
   company, id, name, gender = row
-  if gender == 'M'
- 	gender = 'Male'
-  else
-	gender = 'Female'
-  end
-  list[company+id] = { sid: id,  name:  name, company: company, gender: gender }
+ #  if gender == 'M'
+ # 	gender = 'Male'
+ #  else
+	# gender = 'Female'
+ #  end
+  puts id
+  list[company+' '+id] = { sid: id,  name:  name, company: company, gender: gender }
 end
 
 File.open("list.json","w") do |f|
